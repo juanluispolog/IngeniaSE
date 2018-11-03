89cb27b23f20040eac8dde05adc179ed306744e6
inigogil <44679634+inigogil@users.noreply.github.com>
2018-11-03 17:05:19 +0100

    Add files via upload
    My update

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,29 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+	cout << "!!!Hello World!!" << endl << "Dani is in the mood" << endl; // prints !!!Hello World!!!
+	cout << "What is your name?" << endl;
+	cin >> name;
+	cout << "Hello, " << name << " I´m Sandra" << endl;
+	cout << "Isabel is using GIT" << endl;
+	cout << "Jaime is slower than Isabel" << endl;
+	cout << "Not really, Jaime"<< endl;
+	cout <<"I'm Antonio and I'm bilingual"<<endl;
+	cout <<"I'm Jose and I love INGENIA"<<endl;
+	cout <<"Stop kidding Jaime, I AM JOSE. This is not funny."<<endl;
+	cout <<"David here"<<endl;
+	cout <<"Im Daniel, Error 404 Linux not found." << endl;
+    cout <<"Eclipse is not working on my computer, but i«m still trying. Juan Luis." << endl;
+    cout <<"Im Inigo, how is it going mates?" << endl;
+	return 0;
+}
