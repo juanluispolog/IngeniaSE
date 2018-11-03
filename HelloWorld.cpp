9e24ed09b8cff67063b568622433f3817d88b839
claudiamdc <44679304+claudiamdc@users.noreply.github.com>
2018-11-03 17:39:32 +0100

    Add files via upload

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : DronePose.cpp
+// Name        : HelloWorld.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
@@ -10,6 +10,21 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!!!" << endl; // prints !!!Hello World!!!
+	string name;
+	cout << "!!!Hello World!!" << endl << "Dani is in the mood" << endl; // prints !!!Hello World!!!
+	cout << "What is your name?" << endl;
+	cin >> name;
+	cout << "Hello, " << name << " I´m Sandra" << endl;
+	cout << "Isabel is using GIT" << endl;
+	cout << "Jaime is slower than Isabel" << endl;
+	cout << "Not really, Jaime"<< endl;
+	cout <<"I'm Antonio and I'm bilingual"<<endl;
+	cout <<"I'm Jose and I love INGENIA"<<endl;
+	cout <<"Stop kidding Jaime, I AM JOSE. This is not funny."<<endl;
+	cout <<"David here"<<endl;
+	cout <<"Im Daniel, Error 404 Linux not found." << endl;
+    cout <<"Eclipse is not working on my computer, but i«m still trying. Juan Luis." << endl;
+    cout <<"Im Inigo, how is it going mates?" << endl;
+    cout << "Hi, I'm Claudia" << endl;
 	return 0;
 }
